5be1959e876bee6d01941f809b40622f20cd2c68
Chiranth B R <chiranthgupta11@gmail.com>
2025-11-03 15:50:55 +0530

    First commit and basic server is done

---
+++ server/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader {
+	CheckOrigin: func (r *http.Request) bool  {
+		return true
+	},
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error upgrading: ", err)
+		return
+	}
+	defer conn.Close()
+	
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error reading message! ", err)
+			break
+		}
+
+		fmt.Println("Recieved message! : ", message)
+
+		if err := conn.WriteMessage(websocket.TextMessage, message[:1]); err != nil {
+			fmt.Println("Error sending message to client: ", err)
+			break
+		}
+	}
+
+}
+
+func main() {
+	fmt.Println("Server!")
+	fmt.Println("Server started at localhost:8080!")
+
+	http.HandleFunc("/chat", wsHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server : ", err)
+	}
+}
+
+
+
